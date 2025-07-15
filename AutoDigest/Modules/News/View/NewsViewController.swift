ad079b1c06561c7d1622fe795b849b2f59aefb8b
Wilwous <mick.thomson.tm@gmail.com>
2025-07-15 19:52:17 +0300

    chore: Базовая инициализация проекта, удален storyboard, установлен стартовый контроллер

--- AutoDigest/Modules/News/View/NewsViewController.swift
+++ AutoDigest/Modules/News/View/NewsViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class NewsViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
 }
-
